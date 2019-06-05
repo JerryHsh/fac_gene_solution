7ca0486dd1ce5d4907924693634ff299be1fcb6b
JerryHe <1072436890@qq.com>
2019-06-05 23:55:05 +0800

    rm windows.h

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#include<windows.h>
 #include"myhead.h"
 
 int time_table[gene_num][move_num]={{31,41,25,30},{19,55,3,34},{23,42,27,6},{13,22,14,13},{33,5,57,19}};
